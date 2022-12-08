87a1dbd76f7d865ce0d77a6a1e1e5f8cfea9b03e
bahram <bahram101@mail.ru>
2022-12-08 19:26:23 +0600

    03 inter: Tashkent

---
+++ 03_inter.js
@@ -0,0 +1,17 @@
+
+// const app = document.getElementById("app");
+// const button1 = document.querySelector(".button1");
+
+// const clickHandler = () => {
+//   app.removeChild(button1)
+//   let button2 = document.createElement('button')
+//   let button3 = document.createElement('button')
+//   button2.innerHTML = "Button2"
+//   button3.innerHTML = 'Button3'
+//   app.appendChild(button2)
+//   app.appendChild(button3)
+// };
+
+// button1.addEventListener("click", clickHandler);
+
+/***************************************************************/
