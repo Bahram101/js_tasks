dbf362ff6526827f587942be9292204f5b59e3cf
bahram <bahram101@mail.ru>
2022-12-12 19:33:02 +0600

    changed

---
+++ 07_sudoku.js
@@ -0,0 +1,39 @@
+// let inputs = [
+//   ["4", ".", "1", "."],
+//   [".", "2", ".", "."],
+//   [".", ".", "3", "."],
+//   [".", ".", "2", "."],
+// ];
+
+// let solveSudoku = (board) => {
+//   const size = 4;
+
+//   const findEmpty = (board) => {
+//     for (let r = 0; r < size; r++) {
+//       for (let c = 0; c < size; c++) {
+//         if (board[r][c] === ".") {
+//           return [r, c];
+//         }
+//       }
+//     }
+//   };
+
+//   findEmpty(board);
+// };
+
+// console.log(solveSudoku(inputs));
+
+/**************************************************************/
+
+//Полиндром
+
+const isPolyndrom = (str)=>{
+
+  let revered = str.reverse()
+  console.log(revered);
+
+
+  // return res;
+}
+
+console.log(isPolyndrom)
