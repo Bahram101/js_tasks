2be8937b816b8d98a4a39ab7f36a404eed696185
bahram <bahram101@mail.ru>
2022-12-17 00:25:20 +0600

    added Kavalchuk

---
+++ frontend-science/07_sudoku.js
@@ -0,0 +1,55 @@
+// let inputs = [
+//   ["4", ".", "1", "."],
+//   [".", "2", ".", "."],
+//   [".", ".", "3", "."],
+//   [".", ".", "2", "."],
+// ];
+
+// let solveSudoku = (board) => {
+//   const size = 4;
+
+//   const findEmpty = (board) => {
+//     for (let r = 0; r < size; r++) {
+//       for (let c = 0; c < size; c++) {
+//         if (board[r][c] === ".") {
+//           return [r, c];
+//         }
+//       }
+//     }
+//   };
+
+//   findEmpty(board);
+// };
+
+// console.log(solveSudoku(inputs));
+
+/**************************************************************/
+
+//Полиндром
+
+// const isPalindrome = (str) => {
+//   return str === str.toLowerCase().split("").reverse().join("")
+//     ? "Palindrome"
+//     : "Not Palindrome";
+
+//   //-------------------------------------------------
+
+//   // str = str.toLowerCase().replace(/\s/g, "");
+//   // return str === str.split("").reverse().join("")
+//   //   ? "Palindrome"
+//   //   : "Not Palindrome";
+
+//   //-------------------------------------------------
+
+//   //   const len = Math.floor(str.length / 2);
+//   //   for (let i = 0; i < len; i++) {
+//   //     if (str[i] !== str[str.length - i - 1]) {
+//   //       return "Not Palindrome";
+//   //     }
+//   //   }
+//   //   return "Palindrome";
+// };
+
+// console.log(isPalindrome("racecar"));
+
+
